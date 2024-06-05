2d8c6b031a157ffe478dd3d8947920730330b931
Usuario1 <dcaizatoa.torres@gmail.com>
2024-06-05 00:26:43 -0500

    2nd commit

--- src/main/java/com/example/pruebita/HelloApplication.java
+++ src/main/java/com/example/pruebita/HelloApplication.java
@@ -1,23 +1,16 @@
 package com.example.pruebita;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.awt.*;
+import java.util.Scanner;
+public class HelloApplication {
 
-import java.io.IOException;
+    public static void main (String[]args){
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+        Scanner hola= new Scanner(System.in);
 
-    public static void main(String[] args) {
-        launch();
+        System.out.println("hola pruebita repaso");
+        String pruebita = hola.nextLine();
+
+        System.out.println("Los nombres ingresados son: " + pruebita);
     }
 }
